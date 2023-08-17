96370bf2972e20d77ac2f6780d72cca0eb8e31ef
Juan <jmpasalagua@gmail.com>
2023-08-16 18:52:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import streamlit as st
+import openai
 
-# Mock recipes database
-RECIPES = {
-    "Pasta Aglio e Olio": {"ingredients": ["pasta", "garlic", "olive oil", "red pepper flakes", "parsley"]},
-    "Peanut Butter & Jelly Sandwich": {"ingredients": ["bread", "peanut butter", "jelly"]},
-    "Scrambled Eggs": {"ingredients": ["eggs", "butter", "salt", "pepper"]},
-    "Mashed Potatoes": {"ingredients": ["potatoes", "butter", "milk", "salt"]},
-}
+# Initialize OpenAI GPT-3 client
+openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-def find_recipes(ingredients_list):
-    """Find recipes based on ingredients."""
-    matching_recipes = []
-    for recipe_name, recipe_info in RECIPES.items():
-        if all(ingredient in ingredients_list for ingredient in recipe_info["ingredients"]):
-            matching_recipes.append(recipe_name)
-    return matching_recipes
+def gpt3_recipe_suggestion(ingredients):
+    """Get recipe suggestion from GPT-3 based on ingredients."""
+    prompt_text = f"I have the following ingredients: {', '.join(ingredients)}. What's a simple recipe I can make with them?"
+    response = openai.Completion.create(engine="davinci", prompt=prompt_text, max_tokens=150)
+    return response.choices[0].text.strip()
 
 # Streamlit app
-st.title("Recipe Suggester")
+st.title("GPT-3 Recipe Suggester")
 
 # User input
 ingredients_input = st.text_area("Enter ingredients you have (comma separated):")
@@ -25,12 +19,8 @@ ingredients_list = [ingredient.strip().lower() for ingredient in ingredients_inp
 
 if st.button("Find Recipes"):
     if ingredients_input:
-        suggested_recipes = find_recipes(ingredients_list)
-        if suggested_recipes:
-            st.write("Here are some recipes you can make:")
-            for recipe in suggested_recipes:
-                st.write(f"- {recipe}")
-        else:
-            st.write("Sorry, no matching recipes found for the given ingredients. Try adding more ingredients.")
+        suggested_recipe = gpt3_recipe_suggestion(ingredients_list)
+        st.write("Here's a recipe suggestion from GPT-3:")
+        st.write(suggested_recipe)
     else:
         st.write("Please enter some ingredients.")
